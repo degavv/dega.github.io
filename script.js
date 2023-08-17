e7cd47f3ac239fd9a39523585f65ca7ca519aa50
degavv <48771455+degavv@users.noreply.github.com>
2023-08-17 14:43:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const openPopupButton = document.getElementById('openPopup');
-const closePopupButton = document.getElementById('closePopup');
+const imageContainer = document.querySelector('.boss');
+const imagesInsideContainer = imageContainer.querySelectorAll('img');
 const popup = document.getElementById('popup');
+const popupImage = document.getElementById('popupImage');
+const closePopupButton = document.getElementById('closePopup');
 
-openPopupButton.addEventListener('click', () => {
-  popup.style.display = 'block';
+imagesInsideContainer.forEach(image => {
+  image.addEventListener('click', () => {
+    // При клике на картинку, отображаем всплывающее окно и устанавливаем src картинки
+ //   popupImage.src = image.src;
+    popup.style.display = 'block';
+  });
 });
 
 closePopupButton.addEventListener('click', () => {
